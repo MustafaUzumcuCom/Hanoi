65e42afc35fb34a2984a5bc8347df59de978f341
Brendan Forster <me@brendanforster.com>
2011-10-15 13:11:17 +1030

    started on tests for feature detection

---
+++ source/Hanoi/FeatureDetection.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Hanoi.Serialization.InstantMessaging.Register;
+
+namespace Hanoi
+{
+    public interface IFeatureDetection {
+        
+    }
+
+    public class FeatureDetection : IFeatureDetection
+    {
+        public StreamFeatures Process(Serialization.Core.Streams.StreamFeatures features) {
+
+            StreamFeatures streamFeatures = default(StreamFeatures);
+            if (features.Mechanisms != null && features.Mechanisms.SaslMechanisms.Count > 0)
+            {
+                foreach (string mechanism in features.Mechanisms.SaslMechanisms)
+                {
+                    switch (mechanism)
+                    {
+                        case XmppCodes.SaslDigestMD5Mechanism:
+                            streamFeatures |= StreamFeatures.SaslDigestMD5;
+                            break;
+
+                        case XmppCodes.SaslPlainMechanism:
+                            streamFeatures |= StreamFeatures.SaslPlain;
+                            break;
+
+                        case XmppCodes.SaslXGoogleTokenMechanism:
+                            streamFeatures |= StreamFeatures.XGoogleToken;
+                            break;
+                    }
+                }
+            }
+
+            if (features.Bind != null)
+            {
+                streamFeatures |= StreamFeatures.ResourceBinding;
+            }
+
+            if (features.SessionSpecified)
+            {
+                streamFeatures |= StreamFeatures.Sessions;
+            }
+
+            if (features.Items.Count > 0)
+            {
+                foreach (object item in features.Items)
+                {
+                    if (item is RegisterIQ)
+                    {
+                        streamFeatures |= StreamFeatures.InBandRegistration;
+                    }
+                }
+            }
+
+            return streamFeatures;
+        }
+    }
+}
---
+++ source/Tests/Hanoi.Tests/FeatureDetectionTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace Hanoi.Tests
+{
+    public class FeatureDetectionTests
+    {
+        [Fact]
+        public void Process_ForEmptyStreamCapabilities_ReturnsDefault() {
+            var detection = new FeatureDetection();
+            var features = new Serialization.Core.Streams.StreamFeatures();
+
+            var output = detection.Process(features);
+
+            Assert.Equal(default(StreamFeatures), output);
+        }
+
+        [Fact]
+        public void Process_WithMechanismSet_ReturnsFeature()
+        {
+            var detection = new FeatureDetection();
+            var features = new Serialization.Core.Streams.StreamFeatures();
+            features.Mechanisms.SaslMechanisms.Add(XmppCodes.SaslDigestMD5Mechanism);
+
+            var output = detection.Process(features);
+
+            Assert.True(output.HasFlag(StreamFeatures.SaslDigestMD5));
+        }
+    }
+}
